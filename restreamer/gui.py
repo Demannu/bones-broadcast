ff255f14b828424f726c34571d6d11880eb8a4e8
Mike (Demannu) <michael@gagliano.me>
2022-09-09 00:08:54 -0400

    Audio output and window management updates

--- restreamer/gui.py
+++ restreamer/gui.py
@@ -1,7 +1,9 @@
-from tkinter import Tk, Menu, Frame, Label, Toplevel
+from tkinter import TOP, StringVar, Tk, Menu, Frame, Label, Toplevel
 from threading import Thread
+from tkinter.ttk import Button, Combobox
 import cv2
 from .video_stream import VideoStream
+from .audio_stream import AudioStream
 
 
 class GUI:
@@ -10,23 +12,19 @@ class GUI:
     def __init__(self) -> None:
         # Main Window
         self.root = Tk()
-        self.root.geometry("800x600")
+        # self.root.geometry("400x300")
 
         # Menu Root
         self.menu = Menu(self.root)
 
         # File Menu
         self.filemenu = Menu(self.menu, tearoff=0)
-        self.filemenu.add_command(label="Settings", command=self.settings)
-        self.filemenu.add_separator()
         self.filemenu.add_command(label="Exit", command=self.quit)
 
         self.menu.add_cascade(label="File", menu=self.filemenu)
 
         # Output Menu
         self.outputmenu = Menu(self.menu, tearoff=0)
-        self.outputmenu.add_command(label="Start", command=self.start_video)
-        self.outputmenu.add_command(label="Stop", command=self.stop_video)
         self.outputmenu.add_command(label="Raw Output", command=self.raw_video)
 
         self.menu.add_cascade(label="Output", menu=self.outputmenu)
@@ -37,40 +35,56 @@ class GUI:
         self.display = Frame(self.root, bg="white")
         self.display.grid()
 
-        # Embedded video frame
-        self.video = Label(self.display)
-        self.video.grid()
+        # Video Input Device Configuration
+        video_input_devices = ["Run auto detect"]
+        vid_variable = StringVar(self.display)
+
+        vid_combobox = Combobox(self.display, values=video_input_devices).pack()
+
+        # Video Resolution Configuration
+        video_resolutions = []
+        vr_variable = StringVar(self.display)
+
+        vr_combobox = Combobox(self.display, values=video_resolutions).pack()
+
+        # Auto-detect: Input resolution
+        auto_video_resolution = Button(
+            self.display,
+            text="Auto Detect Resolutions",
+            command=self.auto_detect_video_resolutions,
+        ).pack()
 
         # Capture card output
-        self.input = VideoStream(src=0, container=self.video)
+        self.video_input = VideoStream(src=0)
+        self.audio_input = AudioStream(input_device=2, output_device=4)
+
+        self.video_thread = None
+        self.audio_thread = None
 
         self.root.mainloop()
 
-    def start_video(self):
-        """Start video output inside tkinter window"""
-        self.input.container = self.video
-        self.root.geometry(f"{int(self.input.stream.w)}x{int(self.input.stream.h)}")
-        Thread(target=self.input.start())
-
-    def stop_video(self):
-        """Stop the video output"""
-        self.root.geometry("800x600")
-        self.video.destroy()
-        self.input.stopped = True
+    def auto_detect_video_resolutions(self):
+        pass
 
     def raw_video(self):
         """Stream the raw output to a cv2 window"""
-        self.input.container = None
-        Thread(target=self.input.start())
+        self.display.master.withdraw()
+        self.audio_thread: Thread = Thread(target=self.audio_input.start).start()
+        self.video_thread: Thread = Thread(target=self.video_input.start).start()
+        while not self.video_input.stopped:
+            continue
+        self.display.master.deiconify()
+        self.stop()
+
+    def stop(self):
+        self.audio_input.stop()
+        self.video_input.stop()
+        if self.video_thread and self.audio_thread:
+            self.video_thread.join()
+            self.audio_thread.join()
 
     def quit(self):
         """Exit the program"""
         cv2.destroyAllWindows()
+        self.stop()
         self.root.destroy()
-
-    def settings(self):
-        """Settings window"""
-        new_window = Toplevel(self.root)
-        new_window.title("Settings")
-        new_window.geometry("400x300")
-        Label(new_window, text="Test window").pack()
--- restreamer/video_stream.py
+++ restreamer/video_stream.py
@@ -1,8 +1,9 @@
 from tkinter import Label
+
+import cv2
+
 from .capture_device import CaptureDevice
 from .fps import FPS
-import cv2
-from PIL import Image, ImageTk
 
 
 class VideoStream:  # pylint: disable=too-many-arguments
@@ -14,9 +15,7 @@ class VideoStream:  # pylint: disable=too-many-arguments
         output_fps: int = 60,
         output_w: int = 1920,
         output_h: int = 1080,
-        container: Label = None,
     ):
-        self.container = container
 
         # Setup video capture with DirectShow
         self.stream = CaptureDevice(src=src)
@@ -46,33 +45,10 @@ class VideoStream:  # pylint: disable=too-many-arguments
 
     def loop(self):
         """Render loop; fetch from device; output to window"""
-        if self.stopped:
-            return
-        (self.grabbed, self.frame) = self.stream.cap.read()
-        self.frame = self.fps.render_fps(self.frame)
-        # If we weren't given a tkinter label to display in
-        if not self.container:
+        while not self.stopped:
+            (self.grabbed, self.frame) = self.stream.cap.read()
+            self.frame = self.fps.render_fps(self.frame)
             cv2.imshow("Nintendo Switch", self.frame)
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 self.stop()
             self.fps.update()
-            self.loop()
-        # If we have a tkinter label to display in
-        else:
-            # Convert color scheme from BGR to RGBA
-            cv2im = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGBA)
-
-            # Render frame in PIL format
-            frame = Image.fromarray(cv2im)
-
-            # Render frame in tkinter format
-            frametk = ImageTk.PhotoImage(image=frame)
-
-            # Display frame
-            self.container.imgtk = frametk
-            self.container.configure(image=frametk)
-
-            # Update FPS counter
-            self.fps.update()
-
-            self.container.after(1, self.loop)
